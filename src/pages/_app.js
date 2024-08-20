5c0ff61566062d9750186fe8a017225cfbc80a78
Taeil Kwak <taeil@anvilogic.com>
2024-08-20 07:26:21 -0500

    added auth provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,29 +16,29 @@ export default function App({ Component, pageProps }) {
   }, [])
 
   return (
-    // <Auth0Provider
-    //   domain="dev-bn8s278zc54ocjvv.us.auth0.com"
-    //   clientId="kl6LAPOx7pSTazNZg07jQcfxiXJIdDED"
-    //   authorizationParams={{
-    //     redirect_uri: location,
-    //   }}
-    // >
-    <>
-      <Head>
-        <title>Collabeditor</title>
-        <meta
-          name="description"
-          content="Collaborate on documents with others"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <GlobalStyles>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </GlobalStyles>
-    </>
-    // </Auth0Provider>
+    <Auth0Provider
+      domain="dev-bn8s278zc54ocjvv.us.auth0.com"
+      clientId="kl6LAPOx7pSTazNZg07jQcfxiXJIdDED"
+      authorizationParams={{
+        redirect_uri: location,
+      }}
+    >
+      <>
+        <Head>
+          <title>Collabeditor</title>
+          <meta
+            name="description"
+            content="Collaborate on documents with others"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <GlobalStyles>
+          <main>
+            <Component {...pageProps} />
+          </main>
+        </GlobalStyles>
+      </>
+    </Auth0Provider>
   )
 }
